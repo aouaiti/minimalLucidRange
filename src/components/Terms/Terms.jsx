577f3985a1499505d1b3601d916a0ec984ade31e
Ahmed Aouaiti <94193114+aouaiti@users.noreply.github.com>
2023-11-11 20:38:06 +0100

    Update Terms.jsx

--- src/components/Terms/Terms.jsx
+++ src/components/Terms/Terms.jsx
@@ -1,9 +1,9 @@
 const TermsAndConditions = () => {
   return (
     <div style={containerStyle}>
-      <h1 style={tac}>Lucid Range Print on Demand Terms and Conditions</h1>
+      <h1 style={tac}>WordPunz Print on Demand Terms and Conditions</h1>
       <p>
-        Welcome to Lucid Range Print on Demand ("Lucid Range," "we," "our," or
+        Welcome to WordPunz Print on Demand ("WordPunz," "we," "our," or
         "us"). These terms and conditions ("Terms") outline the rules and
         regulations for the use of our website and services.
       </p>
@@ -16,7 +16,7 @@ const TermsAndConditions = () => {
       <div style={sectionStyle}>
         <h2>1. Introduction</h2>
         <p>
-          1.1. Lucid Range Print on Demand is an online platform that allows
+          1.1. WordPunz Print on Demand is an online platform that allows
           users to create, customize, and showcase personalized designs.
         </p>
         <p>
@@ -28,19 +28,19 @@ const TermsAndConditions = () => {
       <div style={sectionStyle}>
         <h2>2. Design Showcase</h2>
         <p>
-          2.1. Lucid Range is a platform designed for showcasing personalized
-          designs. The designs presented on this website are owned by Lucid
-          Range and intended for display and demonstration purposes only. They
+          2.1. WordPunz is a platform designed for showcasing personalized
+          designs. The designs presented on this website are owned by WordPunz
+          and intended for display and demonstration purposes only. They
           are not available for sale or reproduction through our platform.
         </p>
         <p>
           2.2. Users are allowed to create and customize designs for
           demonstration and preview purposes. However, ownership and copyright
-          of the designs showcased on Lucid Range remain with Lucid Range.
+          of the designs showcased on WordPunz remain with WordPunz.
         </p>
         <p>
           2.3. Users may not use, reproduce, distribute, or modify the showcased
-          designs without the prior written consent of Lucid Range. Any
+          designs without the prior written consent of WordPunz. Any
           unauthorized use of these designs may result in legal action.
         </p>
       </div>
@@ -50,7 +50,7 @@ const TermsAndConditions = () => {
         <p>
           3.1. All intellectual property rights related to our website and
           services, including but not limited to logos, designs, and content,
-          are owned or licensed by Lucid Range.
+          are owned or licensed by WordPunz.
         </p>
         <p>
           3.2. Users may not use, modify, distribute, or reproduce any part of
@@ -70,7 +70,7 @@ const TermsAndConditions = () => {
       <div style={sectionStyle}>
         <h2>5. Changes to Terms and Conditions</h2>
         <p>
-          5.1. Lucid Range reserves the right to modify these Terms at any time
+          5.1. WordPunz reserves the right to modify these Terms at any time
           without prior notice. Updated Terms will be posted on our website.
         </p>
       </div>
@@ -86,7 +86,7 @@ const TermsAndConditions = () => {
       <p>
         By using our website or services, you agree to comply with and be bound
         by these Terms and any future modifications. Thank you for choosing
-        Lucid Range Print on Demand.
+        WordPunz Print on Demand.
       </p>
     </div>
   );
